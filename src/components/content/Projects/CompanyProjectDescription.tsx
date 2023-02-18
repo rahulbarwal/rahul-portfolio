b8b709e5c287c9656ca31e0cf30e84b393054337
Rahul Barwal <rahulbarwal654@gmail.com>
2023-02-18 15:32:53 +0530

    Description with projets done

--- src/components/content/Projects/CompanyProjectDescription.tsx
+++ src/components/content/Projects/CompanyProjectDescription.tsx
@@ -14,8 +14,43 @@ export type ICompanyProject = {
   description: IDescription;
 };
 
-const CompanyProjectDescription = (props: Props) => {
-  return <div>CompanyProjectDescription</div>;
+const CompanyProjectDescription = ({ desc }: Props) => {
+  return (
+    <>
+      {Array.isArray(desc) ? (
+        <section>
+          {desc.map((prj, index) => (
+            <article key={index} className="mt-4">
+              <p>
+                🚀 {prj.projectName}{" "}
+                <span className=" text-slate-500">({prj.duration})</span>
+              </p>
+              <Description desc={prj.description} />
+            </article>
+          ))}
+        </section>
+      ) : (
+        <section>My role</section>
+      )}
+    </>
+  );
 };
 
 export default CompanyProjectDescription;
+
+const Description = ({ desc }: { desc: IDescription }) => {
+  return (
+    <>
+      {desc.texts.map((t) => (
+        <p className=" list-item">{t}</p>
+      ))}
+      <article className="flex gap-2 mt-2">
+        {desc.techStack.map((tech) => (
+          <span className=" bg-gray-500 rounded-2xl py-1 px-2 break-keep">
+            {tech}
+          </span>
+        ))}
+      </article>
+    </>
+  );
+};
