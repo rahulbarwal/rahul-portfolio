d3e9d3920377fc7fb12dec9132d885931fa6d363
Rahul Barwal <rahulbarwal654@gmail.com>
2023-02-18 16:32:22 +0530

    Separated projects data to separate file

---
+++ src/components/content/Projects/data.ts
@@ -0,0 +1,159 @@
+export default [
+  {
+    companyName: "Fluxon",
+    duration: {
+      startedAt: {
+        month: "March",
+        year: "2022",
+      },
+      endedAt: {
+        month: "February",
+        year: "2023",
+      },
+    },
+    description: [
+      {
+        projectName: "Gambit",
+        duration: "1.5 months",
+        description: {
+          texts: ["Implemented time critical designs in 1 month."],
+          techStack: ["React Native", "expo", "firebase"],
+        },
+      },
+      {
+        projectName: "EON",
+        duration: "2 months",
+        description: {
+          texts: [
+            "Migrated complete UI to NextJS in 1.5 months",
+            "Lead the frontend development, delivering critical designs that helped EON’s CEO grab a deal with a major garment exporter.",
+          ],
+          techStack: ["Python", "NextJS", "Docker"],
+        },
+      },
+      {
+        projectName: "OpenBracket",
+        duration: "7.5 months",
+        description: {
+          texts: [
+            "Designed and implemented Code execution in 2 months for openbracket",
+            "Reduced build times by 90% by implementing turborepo in CI/ CD",
+            "Reduced image size by 50 % by using ignite alpine for micro VM.",
+            "Implemented github action to support firebase functions deployment from a monorepo.",
+          ],
+          techStack: [
+            "NextJS",
+            "Firebase",
+            "GCP - Compute",
+            "Cloud run",
+            "Pubsub",
+          ],
+        },
+      },
+    ],
+    position: "Tech Lead",
+  },
+  {
+    companyName: "Globant",
+    duration: {
+      startedAt: {
+        month: "September",
+        year: "2019",
+      },
+      endedAt: {
+        month: "February",
+        year: "2022",
+      },
+    },
+    description: [
+      {
+        projectName: "Login",
+        duration: "11 months",
+        description: {
+          texts: [
+            "Transitioned 16+ markets of HSBC from the legacy Saas platform to DSP. Identified performance bottlenecks in UI rendering, improving performance 3 times.",
+          ],
+          techStack: ["React Native", "expo", "firebase"],
+        },
+      },
+      {
+        projectName: "OLR/OFR",
+        duration: "1 year",
+        description: {
+          texts: [
+            "Enhanced existing functionalities and added 3 online reset options, increasing user engagement by 30%.",
+          ],
+          techStack: ["Python", "NextJS", "Docker"],
+        },
+      },
+      {
+        projectName: "Manage Security",
+        duration: "6 months",
+        description: {
+          texts: [
+            "Developed and deployed the entire project to production for all modules - devices, password, and username. The initial response to this project was good leading to integration into multiple markets",
+            "Resolved 90% of critical production bugs in 3 months.",
+          ],
+          techStack: [
+            "NextJS",
+            "Firebase",
+            "GCP - Compute",
+            "Cloud run",
+            "Pubsub",
+          ],
+        },
+      },
+    ],
+    position: "Tech Lead",
+  },
+  {
+    companyName: "Creyent Arts Pvt ltd",
+    duration: {
+      startedAt: {
+        month: "February",
+        year: "2018",
+      },
+      endedAt: {
+        month: "August",
+        year: "2019",
+      },
+    },
+    description: {
+      texts: [
+        "Participated in all aspects of business development from market research and finance to tech development, and operations. Formulated the company’s vision, goals, and objectives.",
+        "My tech responsibilities revolved around taking architectural decisions for both the backend and frontend.",
+        "Designed and developed an admin Angular web application streamlining high server traffic resource configuration and allocation.",
+        "Designed custom restful design APIs in python to support both mobile app and web app. Integrated Google app engine, Google Cloud SQL, and Cloudinary.",
+        "We onboarded 100+ dance studios(in Pune)and 6000+ individual users onto the platform in the first 6 months.",
+      ],
+      techStack: ["React Native", "expo", "firebase"],
+    },
+    position: "Co founder",
+  },
+  {
+    companyName: "Commvault",
+    duration: {
+      startedAt: {
+        month: "July",
+        year: "2014",
+      },
+      endedAt: {
+        month: "Jan",
+        year: "2018",
+      },
+    },
+    description: [
+      {
+        projectName: "Build Process",
+        duration: "1.5 years",
+        description: {
+          texts: [
+            "Structured several internal systems comprising build and update management applications at Commvault. Added enhancements to the existing system streamlining the continuous integration process, speeding up the build process up to 40%",
+          ],
+          techStack: ["React Native", "expo", "firebase"],
+        },
+      },
+    ],
+    position: "Member of technical staff",
+  },
+];
